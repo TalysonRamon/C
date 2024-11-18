835b2acac7b8ebc85d0cd9327d9beedfb8264f40
Talyson Ramon <talysonramon137@gmail.com>
2024-11-18 20:06:52 -0300

    Create Atividade 046.c

---
+++ Atividades/Atividade 046.c
@@ -0,0 +1,14 @@
+/*
+   Faça um programa para ler um horário (hora:minuto:segundo) de início e a duração,
+   em segundos, de uma experiência biológica. O programa deve informar o horário
+   (hora:minuto:segundo) de término da mesma.
+ */
+
+#include<stdio.h>
+#include<locale.h>
+
+int main()
+{
+    
+    return 0;
+}
