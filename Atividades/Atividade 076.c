1224f7a8029747f41d9b60ab49ed28d5e473940e
Talyson Ramon <talysonramon137@gmail.com>
2024-11-22 11:59:19 -0300

    Create Atividade 076.c

---
+++ Atividades/Atividade 076.c
@@ -0,0 +1,21 @@
+/*
+Faça um programa que realize operações simples de números complexos:
+• Crie e leia dois números complexos, ambos compostos por parte real e parter
+imaginária.
+• Apresente a soma, subtração e produto entre estes dois números complexos.
+• Apresente o módulo dos dois números complexos.
+*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"Portuguese");
+    int u;
+
+    printf("Digite um número complexo: ");
+    scanf("%d", &u);
+    printf("O número complexo é: %d", u);
+    return 0;
+}
