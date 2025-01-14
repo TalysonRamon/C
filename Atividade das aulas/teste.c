6b40cdffb9279fd63cc61dada2d9dfccf7affcf1
Talyson Ramon <talysonramon137@gmail.com>
2025-01-14 15:54:17 -0300

    Update teste.c

--- Atividade das aulas/teste.c
+++ Atividade das aulas/teste.c
@@ -6,14 +6,19 @@
 int main()
 {
     setlocale(LC_ALL,"Portuguese");
-    int a = 1;
+    int a;
 
-    // while para mostrar os números impares de 1 até 100.
-    while (a <= 100)
+    printf("Digite o número 1 ou 2: ");
+    scanf("%d", &a);
+
+    // while para mostrar para testar se o usuario digitou o que foi pedido.
+    while (a != 1 && a != 2)
     {
-        printf("%d ", a);
-        a += 2;
+        printf("Número INCORRETO!! \n");
+        printf("Digite o número novamente: \n");
+        scanf("%d", &a);
     }
     
+    printf("Número lido: %d", a);
     return 0;
 }
