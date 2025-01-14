dde70ad2b06be38ebe54188cc443f101b9d3e63d
Talyson Ramon <talysonramon137@gmail.com>
2025-01-14 09:05:38 -0300

    Update teste.c

--- Atividade das aulas/teste.c
+++ Atividade das aulas/teste.c
@@ -8,9 +8,17 @@ int main()
     setlocale(LC_ALL,"Portuguese");
     int i;
 
+    //incremento
     for ( i = 0; i <= 10; i++)
     {
-        printf("%d\t", i);
+        printf("%d ", i);
+    }
+    printf("\n");
+    
+    //decremento
+    for ( i = 10; i >= 0; i--)
+    {
+        printf("%d ", i);
     }
     
     return 0;
