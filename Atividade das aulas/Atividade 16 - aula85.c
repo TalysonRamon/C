de9502984eed0344c66c2697e7ae5a047e1008aa
Talyson Ramon <talysonramon137@gmail.com>
2025-01-21 11:24:03 -0300

    nova atividade

--- Atividade das aulas/Atividade 16 - aula85.c
+++ Atividade das aulas/Atividade 16 - aula85.c
@@ -9,27 +9,33 @@ números inteiros entre 1 e 10.
 int main()
 {
     setlocale(LC_ALL,"Portuguese");
+
     int cont = 1, i, soma = 0;
 
     do
     {
+        cont++;
+        // for para repitir os números que vão dividir.
         for ( i = 1; i <= 10; i++)
         {
+            // se a divisão for inteira, a variavel soma recebe + 1;
             if (cont % i == 0)
             {
-                soma =+ 1;
-            }
+                soma++;
+            } 
             
         }
+        
+        //Caso o número não tenha 10 divisões inteiras, a variavel soma vai receber 0 e o teste vai começar novamente com outro número, até que o resultado de soma seja igual a 10.
+
         if (soma != 10)
         {
-           soma = 0;
+            soma = 0;
         }
-        cont++;
-    } while (soma == 10);
+        
+    } while (soma != 10);
 
-    printf("");
-    printf("\n%d", cont);
+    printf("\nO menor número divisivel por todos os números de 1 a 10 é: %d", cont);
     
     return 0;
 }
