d957842bce54a0e721bcedcb3efbd7a14beb4226
Talyson Ramon <talysonramon137@gmail.com>
2025-12-30 22:25:18 -0300

    Update teste.c

--- Atividade das aulas/teste.c
+++ Atividade das aulas/teste.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+/*
+                            Lista - Questão 77
+Faça um programa que leia dois pontos correspondentes a dois vetores no plano carte
+siano e calcule o ângulo entre esses dois vetores
+*/
 
 int main()
 {
-    printf("\nOla mundo\n");
-    printf("\nOla mundo\n");
-    printf("\nOla mundo\n");
-    printf("\nOla mundo\n");
+    int x1, y1, x2, y2;
+    printf("Digite 2 valores para o primeiro ponto (x, y): ");
+    scanf("%d%d", &x1, &y1);
+    printf("Digite 2 valores para o segundo ponto (x, y): ");
+    scanf("%d%d", &x2, &y2);
+    
     return 0;
 }
