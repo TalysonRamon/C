2684b3e907456bfc1306ed29ccdd3f1dc95cbd24
Talyson Ramon <talysonramon137@gmail.com>
2026-01-02 09:03:17 -0300

    Update Atividade 080.c

--- Atividades/Atividade 080.c
+++ Atividades/Atividade 080.c
@@ -8,12 +8,21 @@ solo
 
 int main()
 {
-    float massa, distancia, força;
+    float massa, distancia, forca, forca1;
 
     printf("Digite a massa (kg) e a distancia (m): ");
     scanf("%f%f", &massa, &distancia);
 
-    força = 
+    forca = pow(66.7, -11) * (pow(59.72, 24) * massa);
+
+    forca1 = pow(6.371, 2) + pow(distancia, 2);
+
+    if ((forca / forca1) > 100000){
+        printf("A força gravitacional e: %.2f x 10^5", (forca / forca1) / 100000);
+    }
+    else{
+        printf("A força gravitacional e: %.2f", forca / forca1);
+    }
 
     return 0;
 }
