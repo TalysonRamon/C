5c6dffe9d549ab5a33a37fa09c883bc15eddaec8
Talyson Ramon <talysonramon137@gmail.com>
2026-01-02 15:45:33 -0300

    Update Atividade 081.c

--- Atividade 081.c
+++ Atividade 081.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 /*
                         Lista - Questão 081
  Um determinado satélite está a uma altitude h com relação a Terra. Escreva um
@@ -6,8 +7,21 @@ programa que leia a altitude desse satélite e determine sua velocidade orbital 
 período de movimento orbital.
 Dica: utilize as fórmulas e constantes localizadas no Apêndice A
 */
-int main(int argc, char const *argv[])
+int main()
 {
-    
+    float r, T;
+
+    printf("Digite a altitude do satelite: ");
+    scanf("%f", &r);
+
+    const double G = 6.674e-11;
+    const double M = 5.9e24;
+
+    printf("A velocidade orbital do satelite e: %.2e m/s\n", sqrt((G * M) / r ));
+
+    T = 2 * 3.1415 * sqrt(pow(r, 3) / (G * M));
+
+    printf("O periodo de movimento orbital e: %2e seg\n", T);
+
     return 0;
 }
