30278c6f5f9a0b443afcef0e3b7d1c353dff2a02
Talyson Ramon <talysonramon137@gmail.com>
2026-01-05 12:36:43 -0300

    Create Atividade 105.c

---
+++ Atividades/Atividade 105.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/*
+                            Lista - Questão 105
+Faça um programa que:
+• Gere dois números aleatórios a e b inteiros, positivos e no intervalo de [1;100];
+• Escolher aleatoriamente uma das operações artiméticas básicas (soma, subtração,
+divisão ou multiplicação) para ser executada entre os operandos a e b;
+• Pedir ao usuário que forneça o resultado da operação aritmética gerada no ítem
+anterior;
+• Monitorar o tempo de resposta do usuário (tempo entre a momento em que o
+programa exibe a operação aritmética gerada e o momento em que é fornecida a
+resposta);
+• Exibir o resultado entrado pelo usuário; o resultado correto; e o tempo, em segun
+dos, que o usuário levou para responder.
+Dica: use a função time da biblioteca time.h
+*/
+
+int main()
+{
+    unsigned int x, y, esc;
+    float resp;
+
+    srand(time(NULL));
+
+    printf("\nTeste de inteligencia: \n");
+
+    esc = rand();
+    esc = esc % 4;
+
+    switch (esc)
+    {
+    case 0:
+        printf("\nSoma:\n");
+
+        x = rand();
+        y = rand();
+
+        x = x % 100;
+        y = y % 100;
+
+        printf("Resolva a soma a seguir: \n");
+        printf("%d + %d = ", x, y);
+        scanf("%f", &resp);
+
+        if (resp == (x + y))
+            printf("Resposta correta!!\n");
+        else
+            printf("Resposta incorreta!!");
+        break;
+    case 1: 
+        printf("\nSubtracao:\n");
+
+        x = rand();
+        y = rand();
+
+        x = x % 100;
+        y = y % 100;
+        int resul = x - y;
+        printf("Resolva a subtracao a seguir: \n");
+        printf("%d - %d = ", x, y);
+        scanf("%f", &resp);
+
+        if (resp == resul)
+            printf("Resposta correta!!\n");
+        else
+            printf("Resposta incorreta!!");
+        break;
+    case 2:
+        printf("\nDivisao:\n");
+
+        x = rand();
+        y = rand();
+
+        x = x % 100;
+        y = y % 100;
+        float resu = x / y;
+
+        printf("Resolva a divisao a seguir (resultado inteiro da divisao): \n");
+        printf("%d / %d = ", x, y);
+        scanf("%f", &resp);
+
+        if (resp == resu)
+            printf("Resposta correta!!\n");
+        else
+            printf("Resposta incorreta!!");
+
+        break;
+    case 3:
+        printf("\nMultiplicacao:\n");
+
+        x = rand();
+        y = rand();
+
+        x = x % 100;
+        y = y % 100;
+        float resu1 = x * y;
+
+        printf("Resolva a multiplicacao a seguir: \n");
+        printf("%d * %d = ", x, y);
+        scanf("%f", &resp);
+
+        if (resp == resu1)
+            printf("Resposta correta!!\n");
+        else
+            printf("Resposta incorreta!!");
+        break;
+    default:
+        printf("ERROR!!");
+        break;
+    }
+    
+    return 0;
+}
