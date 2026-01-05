43a35709a4e8a535a837e1d7c1989bd53207a07a
Talyson Ramon <talysonramon137@gmail.com>
2026-01-05 10:03:21 -0300

    Create Atividade 104.c

---
+++ Atividades/Atividade 104.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<limits.h>
+/*
+                            Lista - Questão 104
+Faça um programa que informe qual o menor e maior valor que podem ser armazenados
+em uma variável inteira.
+Dica: pesquise a respeito na biblioteca limits.h
+*/
+
+int main()
+{
+    printf("Maior valor armazenado na variavel ineteira %d\nMaior valor armazenado na variavel ineteira %d", INT_MAX, INT_MIN);
+    
+    return 0;
+}
