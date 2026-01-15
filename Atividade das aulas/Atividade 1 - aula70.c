2e77839005b583ab63bd037267317d838bd93ed7
Talyson Ramon <talysonramon137@gmail.com>
2026-01-14 21:54:09 -0300

    Update Atividade 1 - aula70.c

--- Atividade das aulas/Atividade 1 - aula70.c
+++ Atividade das aulas/Atividade 1 - aula70.c
@@ -1,29 +1,26 @@
 /*
+                            Aula - Questão 1
  Elabore um programa em C para ler do teclado um valor inteiro entre 1 e 10 e apresentar a
 tabuada.
 */
 
 #include<stdio.h>
-#include<locale.h>
 
 int main()
 {
-    setlocale(LC_ALL,"Portuguese");
     int num, i = 1;
 
-    printf("\nCALCULADORA: \n");
+    printf("\nTabuada: \n");
     //do while para controlar o que o usúario vai digitar.
     do
     {
-        printf("Digite um número entre 1 e 10: ");
+        printf("Digite um numero entre 1 e 10: ");
         scanf("%d", &num);
     } while (num < 1 || num > 10);
     
-    // While para calcular as multiplicaçôes.
-    while (i <= 10)
-    {
-        printf("%d x %d = %d \n", num, i, num * i);
-        i++;
+    // for para calcular as multiplicaçôes.
+    for ( i = 1; i <= 10; i++){
+        printf("%d x %d = %d\n", num, i, num * i);
     }
     
     return 0;
