c8b7559b77f35a35d01c81fab8e2f327a27dce6c
Talyson Ramon <talysonramon137@gmail.com>
2026-01-22 22:27:14 -0300

    create atividade 211

---
+++ Atividades/Atividade 211.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*
+                            Lista - Questão 211
+Se os números de 1 a 5 são escritos em palavras: um, dois, três, quatro, cinco, então há
+3+4+4+6+5 = 22 letras usadas no total. Faça um programa que conte quantas letras
+seriam utilizadas se todos os números de 1 a 1000 (mil) forem escritos em palavras.
+Obs.: Não conte espaços ou hifens.
+*/ 
+
+int main()
+{
+    um, dois, tres, quatro, cinco, seis, sete, oito, nove // 2+4+4+6+5+4+4+4+4 == 37
+    return 0;
+}
---
+++ Atividades/Atividade 212.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+Faça um programa que leia um número inteiro positivo N e imprima todos os números
+naturais de 0 até N em ordem crescente e decrescente.
+*/
+
+int main()
+{
+    
+    return 0;
+}
