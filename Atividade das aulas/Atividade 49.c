b50ea786c5eb9f35c7d7d07f28108b946b932158
Talyson Ramon <talysonramon137@gmail.com>
2026-02-04 13:17:30 -0300

    Create Atividade 49.c

---
+++ Atividade das aulas/Atividade 49.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/*
+                            Aula - Questão 49 | Lista IV
+Faça um programa para ordenar um vetor com 100 números inteiros. Imprima o vetor antes e
+após a ordenação.
+*/
+
+int main()
+{
+    int v1[100], i, j, aux = 0;
+
+    srand(time(NULL));
+
+    for ( i = 0; i < 100; i++){
+        v1[i] = rand() % 1000;
+    }
+    
+    for ( i = 0; i < 100; i++){
+        for ( j = 0; j < 99; j++){
+            if (v1[j] > v1[j + 1]){
+                aux = v1[j + 1];
+                v1[j + 1] = v1[j];
+                v1[j] = aux;
+            }
+            
+        }
+        
+    }
+    
+    printf("\n");
+    for ( i = 0; i < 100; i++){
+        printf("%d ", v1[i]);
+    }
+    
+    return 0;
+}
