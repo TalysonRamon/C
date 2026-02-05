0dc7c84c5245359c9a741cc36cb2180f9b2dff1f
Talyson Ramon <talysonramon137@gmail.com>
2026-02-05 08:28:00 -0300

    Create Atividade 53 - aula.c

---
+++ Atividade das aulas/Atividade 53 - aula.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/*
+                    Aula - Questão 53 | Lista IV
+Faça um programa que imprima na tela a diagonal principal de uma matriz 5 x 5.
+*/
+
+int main()
+{
+    int m[5][5], i, j, n = 4;
+
+    srand(time(NULL));
+
+    for ( i = 0; i < 5; i++){
+        for ( j = 0; j < 5; j++){
+            m[i][j] = rand() % 1000;
+            printf("%4d ", m[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\nDiagonal Principal:\n\n");
+    for ( i = 0; i < 5; i++){
+        printf(" %4d ", m[i][i]);
+        printf("\n");
+        for ( j = n; j <= 4; j++){
+            printf("    ");
+        }    
+        n--;
+    }
+    
+    return 0;
+}
