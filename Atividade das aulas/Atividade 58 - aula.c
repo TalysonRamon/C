3a10c8dddd3b37901a5b7e56a2c18a045352fd4c
Talyson Ramon <talysonramon137@gmail.com>
2026-02-07 10:52:58 -0300

    Create Atividade 58 - aula.c

---
+++ Atividade das aulas/Atividade 58 - aula.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*
+                            Aula - Questão 16 | Lista IV
+Faça um programa para jogar o jogo da velha. Ao final imprima o resultado do jogo e pergunte
+se deseja jogar novamente.
+*/
+
+int main()
+{
+
+    printf("__|_____|__\n__|_____|__\n  |     | \n");
+    printf("[0][0]  [0][1]  [0][2]\n[1][0]  [1][1]  [1][2]\n[2][0]  [2][1]  [2][2]");
+
+    return 0;
+}
