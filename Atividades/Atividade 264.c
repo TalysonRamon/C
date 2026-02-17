bdeb80a1cf5250d3ccbf9dc825439da6301508c0
Talyson Ramon <talysonramon137@gmail.com>
2026-02-17 19:54:42 -0300

    Create Atividade 264.c

---
+++ Atividades/Atividade 264.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+/*
+                            Lista - Questão 264
+Faça um programa que leia 20 valores inteiros correspondente a idade de uma turma
+de alunos. Armazene esses dados em um vetor. Calcule a média e o desvio padrão dos
+valores do vetor. Mostre também a maior e a menor idade armazenada no vetor.
+*/
+
+int main()
+{
+    float media = 0, dp = 0;
+    int v[20], i, soma = 0, cont = 1;
+
+    for ( i = 0; i < 20; i++){
+        printf("Digite a idade do aluno %d: ", cont++);
+        scanf("%d", &v[i]);
+        soma += v[i];
+    }
+    
+    media = soma / 20.0;
+
+    for ( i = 0; i < 20; i++){
+        dp += pow(v[i] - media, 2);
+    }
+    
+    printf("\nMedia: %.2f\nDesvio padrao: %.2f", media, sqrt(dp / 20.0));
+    return 0;
+}
