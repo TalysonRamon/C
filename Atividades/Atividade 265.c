aea5ef9cf94b6c8a543ddcc8259b88ce8476ccac
Talyson Ramon <talysonramon137@gmail.com>
2026-02-19 22:39:40 -0300

    Update Atividade 265.c

--- Atividades/Atividade 265.c
+++ Atividades/Atividade 265.c
@@ -20,12 +20,39 @@ candidatos que deverão concorrer.
 
 int main()
 {
-    int v[10];
+    int votantes, op, i, v[10] = {};
 
-    for ( i = 0; i < count; i++)
-    {
-        /* code */
+    printf("Qual a quantidade de votantes: ");
+    scanf("%d", &votantes);
+
+    for ( i = 1; i <= votantes; i++){
+        printf("Digite:\n13134 - Para votar no Ademir\n97873 - Para votar na Maria\n48828 - Para votar no Jorge\n47872 - Para votar no Leo\n");
+        printf("98953 - Para votar na Joana\n58959 - Para votar no Jao\n87342 - Para votar no Marcelo\n89472 - Para votar no Wagner\n");
+        printf("78634 - Para votar na Larissa\n84972 - Para votar no Lopes\n0 - Para votar em branco\nQualquer outro numero para voto nulo\n");
+        printf("Opcao: ");
+        scanf("%d", &op);
+        switch (op)
+        {
+        case 13134:
+            v[0]++;
+            break;
+        case 97873:
+            v[1]++;
+            break;
+        case 48828:
+            v[2]++;
+            break;
+        case 47872:
+            v[3]++;
+            break;
+        case 98953:
+            v[4]++;
+            break;
+        default:
+            break;
+        }
     }
+    
     
     return 0;
 }
